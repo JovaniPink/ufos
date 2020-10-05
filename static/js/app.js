2c2631eb7a235f171e61259a02705407a18d34fd
Jovani Pink <jovanirosado@hotmail.com>
2020-10-04 20:33:18 -0400

    Prepping to make changes to XState

--- static/js/app.js
+++ static/js/app.js
@@ -1,8 +1,8 @@
 // Import Bootstrap
-import "bootstrap";
+import 'bootstrap';
 
 // Importing the custom scss
-import "../scss/style.scss";
+import '../scss/style.scss';
 
 // Importing D3
 // We do not have to import all of D3, only the selection module.
@@ -10,33 +10,12 @@ import "../scss/style.scss";
 // import * as d3 from "../../node_modules/d3-selection/dist/d3-selection.js";
 
 // Importing the data from data.js
-import { data } from "./data.js";
+import { data } from './data.js';
 const tableData = data;
 
 // Reference DOM elements
-let tbody = d3.select("tbody");
+let tbody = d3.select('tbody');
 let h4 = d3.select("h4[id='resultCount']");
-
-// Build the Table from UFO data
-function buildTable(data) {
-  tbody.html("");
-  h4.html("");
-  h4.text("No of Rows Displayed: " + data.length);
-
-  // Next, loop through each object in the data
-  // and append a row and cells for each value in the row
-  data.forEach((dataRow) => {
-    // Append a row to the table body
-    let row = tbody.append("tr");
-
-    // Loop through each field in the dataRow and add
-    // each value as a table cell (td)
-    Object.values(dataRow).forEach((val) => {
-      let cell = row.append("td");
-      cell.text(val);
-    });
-  });
-}
 
 // Filter options
 let filters = {};
@@ -44,20 +23,15 @@ let filters = {};
 // Function to update the filters.
 function updateFilters() {
   let changedElement = d3.select(this);
-  let elementValue = changedElement.property("value");
-  let filterId = changedElement.attr("id");
+  let elementValue = changedElement.property('value');
+  let filterId = changedElement.attr('id');
 
-  if (elementValue) {
-    filters[filterId] = elementValue;
-  } else {
-    delete filters[filterId];
-  }
-
-  // elementValue ? filters[filterId] = elementValue : delete filters[filterId];
+  elementValue ? (filters[filterId] = elementValue) : delete filters[filterId];
 
   filterTable();
 }
 
+// Build the new filtered table
 function filterTable() {
   let filterTable = tableData;
 
@@ -68,6 +42,29 @@ function filterTable() {
   buildTable(filterTable);
 }
 
-d3.selectAll("input").on("change", updateFilters);
+// Build the Table from UFO data
+function buildTable(data) {
+  tbody.html('');
+  h4.html('');
+  h4.text('No of Rows Displayed: ' + data.length);
+
+  // Next, loop through each object in the data
+  // and append a row and cells for each value in the row
+  data.forEach((dataRow) => {
+    // Append a row to the table body
+    let row = tbody.append('tr');
+
+    // Loop through each field in the dataRow and add
+    // each value as a table cell (td)
+    Object.values(dataRow).forEach((val) => {
+      let cell = row.append('td');
+      cell.text(val);
+    });
+  });
+}
+
+// https://github.com/d3/d3-selection/blob/v2.0.0/README.md#selectAll
+// https://github.com/d3/d3-selection/blob/v2.0.0/README.md#selection_on
+d3.selectAll('input').on('change', updateFilters);
 
 buildTable(tableData);
